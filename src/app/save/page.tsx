e48a94e39325d28a938e7dac9e2a74df6754aa60
Leewonno <dldnjssh123@naver.com>
2024-10-21 19:28:52 +0900

    [Feat] Save 페이지

--- src/app/save/page.tsx
+++ src/app/save/page.tsx
@@ -1,13 +1,25 @@
+import executeQuery from "../lib/db"
+
 // 날짜 입력되면 해당 날짜의 데이터를 크롤링해서 DB에 저장
 // 날짜 - input
 // DB에 저장된 데이터 미리 확인
 
-export default function Save() {
-    return (
-      <div>
-        {/* 데이터 크롤링 후 저장 */}
-        저장
-      </div>
-    );
-  }
-  
+export default async function Save() {
+  const sql = 'select * from save'
+  const data = await executeQuery(sql, '')
+  const getdata = JSON.parse(JSON.stringify(data))
+  console.log(getdata)
+  return (
+    <div>
+      {getdata.map((data: any, i: any) => {
+        return (
+          <>
+            <p>
+              {data.id} {data.title}
+            </p>
+          </>
+        )
+      })}
+    </div>
+  )
+}
