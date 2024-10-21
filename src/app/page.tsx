4b927d6af959102f0fb0c1b242dfd48ff34e26ec
Leewonno <dldnjssh123@naver.com>
2024-10-21 18:20:34 +0900

    [Feat] Home 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import styles from "./page.module.css";
+'use client'
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 35px;
+`
+
+const LogoBox = styled.div`
+  display: flex;
+  gap: 10px;
+`
+
+const Logo = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+`
+
+const LinkBox = styled.div`
+  display: flex;
+  gap: 10px;
+`
+
+const LinkButton = styled(Link)`
+  font-size: 25px;
+  text-decoration: none;
+  color: #000;
+  padding: 5px 15px;
+  border: 1px solid #dedede;
+  border-radius: 15px;
+`
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      StockHelper
-    </div>
+    <Box>
+      <LogoBox>
+        <Logo>STOCK HELPER</Logo>
+      </LogoBox>
+      <LinkBox>
+        <LinkButton href='/save'>저장</LinkButton>
+        <LinkButton href='/analyze'>분석</LinkButton>
+      </LinkBox>
+    </Box>
   );
 }
---
+++ src/app/save/page.tsx
@@ -0,0 +1,9 @@
+export default function Save() {
+    return (
+      <div>
+        {/* 데이터 크롤링 후 저장 */}
+        저장
+      </div>
+    );
+  }
+  
